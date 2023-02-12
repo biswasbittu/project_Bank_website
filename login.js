87494bcdf93275af71011f01b0cf574df1fdb479
biswasbittu <biswasbittu7415@yahoo.com>
2023-02-12 11:27:22 +0530

    login button working

--- login.js
+++ login.js
@@ -1,20 +1,21 @@
-document.getElementById('btn-submit').addEventListener('click',function(){
-   const emailField= document.getElementById('user-email');
-   const emailUpper=emailField.value;
-   const email=emailUpper.toLowerCase();
+document.getElementById('btn-submit').addEventListener('click', function () {
+    const emailField = document.getElementById('user-email');
+    const emailUpper = emailField.value;
+    const email = emailUpper.toLowerCase();
 
 
 
-//    Password Field
-const passwordField=document.getElementById('user-password')
-const password=passwordField.value;
+    //    Password Field
+    const passwordField = document.getElementById('user-password')
+    const password = passwordField.value;
 
-if(email==='biswasbittu@gmail.com' && password ==='741501' )
-{
-    console.log("valid User")
-}
-
-else{
-    console.log("Invalid User")
-}
+    if (email === 'biswasbittu@gmail.com' && password === '741501') {
+        location.href='./bank.html';
+    }
+     else if(email !== 'biswasbittu@gmail.com'){
+        alert("Invalid Username")
+    }
+    else {
+        alert('Invalid Password');
+    }
 })
